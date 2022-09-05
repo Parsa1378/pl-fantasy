a345bcb076729ebbbea6a03c3954efd0f6f41283
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-05 23:13:33 +0430

    team model added

---
+++ src/app/models/team.model.ts
@@ -0,0 +1,45 @@
+import { ObjectId, Schema, model, Types } from "mongoose";
+
+interface Team {
+  picks: [
+    {
+      player_id: ObjectId;
+      multiplier: Number;
+      is_captain: Boolean;
+      is_vice_captain: Boolean;
+    }
+  ];
+}
+
+const teamSchema = new Schema<Team>(
+  {
+    picks: [
+      {
+        player: {
+          type: Types.ObjectId,
+          ref: "Player",
+          default: null,
+        },
+        multiplier: {
+          type: Number,
+          default: 1,
+        },
+        is_captain: {
+          type: Boolean,
+          default: false,
+        },
+        is_vice_captain: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Team = model("Team", teamSchema);
+
+module.exports = Team;
