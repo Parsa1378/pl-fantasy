d423c8de197bf0f6dc619153225b837d7449e0dc
Parsa Poorsistani <poorsistani13@gmail.com>
2022-09-05 23:18:07 +0430

    path of models  added

---
+++ src/app/models/path.ts
@@ -0,0 +1,15 @@
+const playerModel = require("./playerModel");
+const positionModel = require("./positionModel");
+const managerModel = require("./managerModel");
+const teamModel = require("./teamModel");
+const eventModel = require("./eventModel");
+const pl_teamModel = require("./pl-teamModel");
+
+export {
+  playerModel,
+  positionModel,
+  managerModel,
+  teamModel,
+  eventModel,
+  pl_teamModel,
+};
